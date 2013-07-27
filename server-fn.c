ba361c8fe9c70d013ee275a712986f71d3cd9de0
Thomas Adam <thomas@xteddy.org>
2013-07-27 12:53:50 +0100

    Grouped session: Don't crash on resize calculations
    When a grouped session is destroyed, don't unconditionally try and
    recalculate the sizes of the window, because it's possible the other session
    might also be destroyed as well, if the window is last one in the session.
    Doing this unconditionally means we're then accessing dangling pointers.

--- server-fn.c
+++ server-fn.c
@@ -450,9 +450,10 @@ server_destroy_session(struct session *s)
 			notify_attached_session_changed(c);
 			session_update_activity(s_new);
 			server_redraw_client(c);
+
+			recalculate_sizes();
 		}
 	}
-	recalculate_sizes();
 }
 
 void
