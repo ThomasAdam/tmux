785003ec0ff4339b2bd9af7e6b214d088ab464fa
Nicholas Marriott <nicholas.marriott@gmail.com>
2015-01-29 12:50:37 +0000

    Convert send-keys to prep flags.

--- cmd-send-keys.c
+++ cmd-send-keys.c
@@ -33,7 +33,7 @@ const struct cmd_entry cmd_send_keys_entry = {
 	"send-keys", "send",
 	"lRt:", 0, -1,
 	"[-lR] " CMD_TARGET_PANE_USAGE " key ...",
-	0,
+	CMD_PREP_PANE_T,
 	cmd_send_keys_exec
 };
 
@@ -41,7 +41,7 @@ const struct cmd_entry cmd_send_prefix_entry = {
 	"send-prefix", NULL,
 	"2t:", 0, 0,
 	"[-2] " CMD_TARGET_PANE_USAGE,
-	0,
+	CMD_PREP_PANE_T,
 	cmd_send_keys_exec
 };
 
@@ -49,14 +49,11 @@ enum cmd_retval
 cmd_send_keys_exec(struct cmd *self, struct cmd_q *cmdq)
 {
 	struct args		*args = self->args;
-	struct window_pane	*wp;
-	struct session		*s;
+	struct window_pane	*wp = cmdq->state.tflag.wp;
+	struct session		*s = cmdq->state.tflag.s;
 	struct input_ctx	*ictx;
 	const u_char		*str;
 	int			 i, key;
-
-	if (cmd_find_pane(cmdq, args_get(args, 't'), &s, &wp) == NULL)
-		return (CMD_RETURN_ERROR);
 
 	if (self->entry == &cmd_send_prefix_entry) {
 		if (args_has(args, '2'))
