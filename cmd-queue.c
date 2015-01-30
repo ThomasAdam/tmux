6cbbafcbc38ad75c71c4a4c0050614a043b4cf05
Nicholas Marriott <nicholas.marriott@gmail.com>
2015-01-30 18:51:16 +0000

    Hooks need to go between guards too.

--- cmd-queue.c
+++ cmd-queue.c
@@ -196,14 +196,17 @@ cmdq_continue(struct cmd_q *cmdq)
 			log_debug("cmdq %p: %s (client %d)", cmdq, s,
 			    cmdq->client != NULL ? cmdq->client->ibuf.fd : -1);
 
-			if (cmd_prepare_state(cmd, cmdq) != 0)
-				break;
-
 			cmdq->time = time(NULL);
 			cmdq->number++;
 
 			flags = !!(cmd->flags & CMD_CONTROL);
 			guard = cmdq_guard(cmdq, "begin", flags);
+
+			if (cmd_prepare_state(cmd, cmdq) != 0) {
+				if (guard)
+					cmdq_guard(cmdq, "error", flags);
+				break;
+			}
 
 			if (cmdq->state.tflag.s != NULL)
 				hooks = &cmdq->state.tflag.s->hooks;
@@ -218,22 +221,20 @@ cmdq_continue(struct cmd_q *cmdq)
 			 * it needs to be restored afterwards. XXX not very
 			 * obvious how this works from here...
 			 */
-			if (cmd_prepare_state(cmdq->cmd, cmdq) != 0)
-				break;
-
-			retval = cmdq->cmd->entry->exec(cmdq->cmd, cmdq);
-			if (retval != CMD_RETURN_ERROR)
-				cmdq_run_hook(hooks, "after", cmdq->cmd, cmdq);
-
-			if (guard) {
-				if (retval == CMD_RETURN_ERROR)
+			if (cmd_prepare_state(cmd, cmdq) != 0)
+				retval = CMD_RETURN_ERROR;
+			else
+				retval = cmd->entry->exec(cmd, cmdq);
+			if (retval == CMD_RETURN_ERROR) {
+				if (guard)
 					cmdq_guard(cmdq, "error", flags);
-				else
-					cmdq_guard(cmdq, "end", flags);
-			}
-
-			if (retval == CMD_RETURN_ERROR)
 				break;
+			}
+			cmdq_run_hook(hooks, "after", cmd, cmdq);
+
+			if (guard)
+				cmdq_guard(cmdq, "end", flags);
+
 			if (retval == CMD_RETURN_WAIT)
 				goto out;
 			if (retval == CMD_RETURN_STOP) {
